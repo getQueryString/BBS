45f5fcf2e856a26144a489f0f4b6da9cff79b79d
getQueryString <fi.l@tutanota.com>
2024-02-22 14:44:45 +0100

    Programming from computer science lessons at school.

--- src/Primzahlen.java
+++ src/Primzahlen.java
@@ -1,21 +1,35 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class Primzahlen {
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) {
+        int primeCount = 0, arrayCount = 0, arrayCount2 = 1;
+        long startTime = System.currentTimeMillis(), stepTime, stepElapsedTime;
         boolean isPrime;
-        FileWriter writer = new FileWriter("prime.txt");
-        for (int num = 2; num < 100; num++) {
+        ArrayList<Long> timeArray = new ArrayList<>();
+        for (int num = 1; num <= 100000; num += 2) {   // 1999990000    2000000000
             isPrime = true;
-            for (int i = 2; i < num; i++) {
+            arrayCount++;
+            for (int i = 2; i < num / 2; i++) {
                 if (num % i == 0) {
                     isPrime = false;
                     break;
                 }
             }
-            if (isPrime) writer.write(num + ", ");
+            if (isPrime) {
+                //System.out.println(num);
+                if (arrayCount % arrayCount2 == 0) {
+                    arrayCount2 *= 10;
+                    stepTime = System.currentTimeMillis();
+                    stepElapsedTime = stepTime - startTime;
+                    timeArray.add(stepElapsedTime);
+                }
+                primeCount++;
+            }
         }
-        writer.flush();
-        writer.close();
+        System.out.println("\nListe der Zeiten: " + timeArray);
+        long endTime = System.currentTimeMillis();
+        long elapsedTimeMS = endTime - startTime;
+        double elapsedTimeS = (double) (endTime - startTime) / 1000;
+        System.out.println(primeCount + " Primzahlen gefunden (exklusive 2). Der Vorgang hat insg. " + elapsedTimeMS + " ms (" + elapsedTimeS + " s) gedauert.");
     }
 }
